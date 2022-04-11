61f1a7cb6dc16dd1f7216a761395798c1f2dafba
Beniamino <beniaminovagnarelli@gmail.com>
2022-04-11 22:22:55 +0200

    correzioni

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,32 +11,61 @@
 
     <title>CTF h4ckus4t1</title>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="preload">
     <nav id="navbar">
         <div id="navbar-logo">
             <img src = "img/logo.svg" alt="My Happy SVG" style="height: 100px;">
         </div>
         <div id="navbar-menu">
-            <button id="navbar-menu-button" onclick="navbar_menu_button()">
-                Menu
-            </button>
+            <button id="navbar-menu-button" onclick="navbar_menu_button()"></button>
         </div>
         <div id="navbar-nav">
-            <a href="">Home</a>
-            <a href="">Classifica</a>
-            <a href="">Challenges</a>
+            <a href="">
+                <img src="img/home.svg" width="20">
+                Home
+            </a>
+            <a href="">
+                <img src="img/scoreboard.svg" width="20">
+                Classifica
+            </a>
+            <a href="">
+                <img src="img/flag.svg" width="20">
+                Challenges
+            </a>
         </div>
-        <div id="navbar-login">
-            <button>
+        <div id="navbar-login-menu">
+            <button id="login-button">
                 Login
             </button>
+            <button id="register-button">
+                Register
+            </button>
+            <!-- <button id="account-button">
+                Account
+            </button>
+            <button id="logout-button">
+                Logout
+            </button> -->
         </div>
         
     </nav>
     <div id="main">
-        Main
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+Where does it come from?
+
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+Where can I get some?
+
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
     </div>
     <div id="footer">Footer</div>
 
--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ function navbar_menu_button(change_state = true) {
         navbar_menu_button.state = !navbar_menu_button.state;
 
     if (navbar_menu_button.state == true) {
-        navbar_nav.style.display = "";
-        navbar_login.style.display = "";
+        navbar_nav.classList.remove("hidden");
+        navbar_login_menu.classList.remove("hidden");
+
     } else {
-        navbar_nav.style.display = "none";
-        navbar_login.style.display = "none";
+        navbar_nav.classList.add("hidden");
+        navbar_login_menu.classList.add("hidden");
     }
+
+    // main.style.minHeight = "calc(100vh - " + (parseInt(navbar.clientHeight) + parseInt(footer.clientHeight)) + "px)";
 }
 
 function mq_width_listener(mq){
@@ -16,35 +19,42 @@ function mq_width_listener(mq){
         navbar_logo.style.width = "50%";
 
         navbar_nav.style.width = "100%";
-        navbar_nav_links.forEach(link => link.style.width = "100%");
-        navbar_nav_links.forEach(link => link.style.textAlign = "left");
+        navbar_nav.style.textAlign = "left";
 
-        navbar_login.style.width = "100%";
-        navbar_login.style.textAlign = "center";
+        navbar_login_menu.style.width = "100%";
+        navbar_login_menu.style.textAlign = "center";
 
         navbar_menu_button(false);
     }
     else {
         navbar_logo.style.width = "30%";
 
-        navbar_nav.style.display = "";
+        navbar_nav.classList.remove("hidden");
         navbar_nav.style.width = "40%";
         navbar_nav.style.textAlign = "center";
-        navbar_nav_links.forEach(link => link.style.flex = "");
 
-        navbar_login.style.display = "";
-        navbar_login.style.width = "30%";
-        navbar_login.style.textAlign = "right";
+        navbar_login_menu.classList.remove("hidden");
+        navbar_login_menu.style.width = "30%";
+        navbar_login_menu.style.textAlign = "right";
+        
+        // main.style.minHeight = "calc(100vh - " + (parseInt(navbar.clientHeight) + parseInt(footer.clientHeight)) + "px)";
     }
 }
 
-navbar_menu_button.state = false;
+let body = document.querySelector("body");
+let navbar = document.querySelector("#navbar");
+let navbar_logo = navbar.querySelector("#navbar-logo");
+let navbar_nav = navbar.querySelector("#navbar-nav");
+let navbar_login_menu = navbar.querySelector("#navbar-login-menu");
+let main = document.querySelector("#main");
+let footer = document.querySelector("#footer");
 
-let navbar_logo = document.querySelector("#navbar-logo");
-let navbar_nav = document.querySelector("#navbar-nav");
-let navbar_nav_links = navbar_nav.querySelectorAll("a");
-let navbar_login = document.querySelector("#navbar-login");
+navbar_menu_button.state = false;
   
 let mq = window.matchMedia("(max-width: 767px)");
-mq_width_listener(mq) // Call listener function at run time
+mq_width_listener(mq)
 mq.addEventListener("change", mq_width_listener);
+
+window.onload = function() {
+    body.classList.remove("preload");
+}
