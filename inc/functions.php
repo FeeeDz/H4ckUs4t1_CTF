12eea4cec02fe67da24f237c32d3ec083b0212f2
Beniamino <beniaminovagnarelli@gmail.com>
2022-04-15 17:18:23 +0200

    login

---
+++ inc/functions.php
@@ -0,0 +1,57 @@
+<?php
+
+$hash_options = [
+    'cost' => 10,
+];
+
+function db_connect() {
+    require "inc/db_config.php";
+
+    if ($conn = mysqli_connect($hostname, $username, $password, $servername)) 
+        return $conn;
+    return false;
+}
+
+function db_login($conn, $username, $password) {
+
+    $query = "SELECT password_hash, role FROM CTF_user WHERE username = ?";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row && password_verify("***REMOVED***", $row['password_hash'])) {
+        $_SESSION['logged'] = $username;
+        $_SESSION['role'] = $row['role'];
+        return true;
+    }
+
+    unset($_SESSION['logged']);
+    unset($_SESSION['role']);
+    return false;
+}
+
+function db_logout(){
+    unset($_SESSION['logged']);
+    unset($_SESSION['role']);
+    return true;
+}
+
+function db_register($conn, $username, $password, $email) {
+    global $hash_options;
+
+    $password_hash = password_hash($password, PASSWORD_DEFAULT, $hash_options);
+    $query = "INSERT INTO CTF_user (username, password_hash, email, registration_date, last_login, role, team_id)
+        VALUES (?, ?, ?, NOW(), NOW(), 'U', NULL)";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $username, $password_hash, $email);
+
+    if ($stmt->execute())
+        return true;    
+    return false;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,95 +1,30 @@
-<?php
-session_start();
+<?php 
+$title = "CTF h4ckus4t1";
+require "inc/head.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>CTF h4ckus4t1</title>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
-    <link rel="stylesheet" href="css/style.css">
-</head>
 <body>
-    <nav id="navbar">
-        <div id="navbar-logo" style="height: 90px;">
-            <img src = "img/logo.svg">
-        </div>
-        <div id="navbar-menu">
-            <button id="navbar-menu-button" onclick="navbar_menu_button()"></button>
-        </div>
-        <div id="navbar-nav">
-            <a href="">
-                <span id="navbar-nav-home" class="navbar-nav-icon"></span>
-                <span>Home</span>
-            </a>
-            <a href="">
-                <span id="navbar-nav-scoreboard" class="navbar-nav-icon"></span>
-                <span>Classifica</span>
-            </a>
-            <a href="">
-                <span id="navbar-nav-flag" class="navbar-nav-icon"></span>
-                <span>Challenges</span>
-            </a>
-        </div>
-        <div id="navbar-login-menu">
-            <button id="login-button">
-                Login
-            </button>
-            <button id="register-button">
-                Register
-            </button>
-            <button id="account-button">
-                Account
-            </button>
-            <button id="logout-button">
-                Logout
-            </button>
-        </div>
-        
-    </nav>
+    <?php require "inc/navbar.php"; ?>
     <div id="main">
 
-        <?php
-        require "db/db_functions.php";
+    <?php
 
-        $conn = db_connect();
+    require "inc/functions.php";
+    $conn = db_connect();
+    if (db_login($conn, "imBenjamin741", "***REMOVED***")) {
+        echo $_SESSION["role"];
+    }
 
-        echo bin2hex(random_bytes(16));
+    if (db_register($conn, "imBenjamin741bis", "***REMOVED***", "email")) {
+        echo "adsf";
+    }
 
-        ?>
+    // echo bin2hex(random_bytes(16));
 
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-Why do we use it?
+    // echo password_hash("***REMOVED***", PASSWORD_DEFAULT, $hash_options);
 
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-Where does it come from?
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-Where can I get some?
-
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-Why do we use it?
-
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-Where does it come from?
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-Where can I get some?
-
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+    ?>
     </div>
-    <div id="footer">Footer</div>
+    <?php require "inc/footer.php"; ?>
 
     <script src="js/script.js"></script> 
 </body>
