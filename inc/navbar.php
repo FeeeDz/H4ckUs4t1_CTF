2a6f76198b29291e976463819e591fed2b22e46d
Beniamino <beniaminovagnarelli@gmail.com>
2022-04-28 11:28:48 +0200

    push

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,8 +1,8 @@
 <nav id="nav">
     <input type="checkbox" id="nav__checkbox">
     <label for="nav__checkbox" id="nav__toggle">
-        <span class="material-icons">menu</span>
-        <span class="material-icons">close</span>
+        <span id="nav__toggle__menu" class="material-icons">menu</span>
+        <span id="nav__toggle__close" class="material-icons">close</span>
     </label>
     <ul class="nav__menu">
         <li><a href="index.php">
@@ -21,22 +21,22 @@
             <span>Challenges</span>
         </a></li>
     </ul>
-        <!-- <div id="navbar-login-menu">
-        <?php if (isset($_SESSION["logged"])) : ?>
-            <button id="account-button" onclick="location.href = 'account.php'">
-                <?php echo $_SESSION["logged"]; ?>
-            </button>
-            <button id="logout-button" onclick="location.href = 'logout.php'">
-                Logout
-            </button>
-        <?php else : ?>
-            <button id="login-button" onclick="location.href = 'login.php?redirect=<?php echo basename($_SERVER['PHP_SELF']);?>'">
-                Login
-            </button>
-            <button id="register-button" onclick="location.href = 'register.php'">
-                Register
-            </button>
-        <?php endif; ?>
-        </div> -->
+    <div id="navbar-login-menu">
+    <?php if (isset($_SESSION["logged"])) : ?>
+        <button id="account-button" onclick="location.href = 'account.php'">
+            <?php echo $_SESSION["logged"]; ?>
+        </button>
+        <button id="logout-button" onclick="location.href = 'logout.php'">
+            Logout
+        </button>
+    <?php else : ?>
+        <button id="login-button" onclick="location.href = 'login.php?redirect=<?php echo basename($_SERVER['PHP_SELF']);?>'">
+            Login
+        </button>
+        <button id="register-button" onclick="location.href = 'register.php'">
+            Register
+        </button>
+    <?php endif; ?>
+    </div>
     
 </nav>
