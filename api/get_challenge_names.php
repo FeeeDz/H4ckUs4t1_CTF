d39297f309f7e573ab2afc92bf2c1aa3a90ed110
Beniamino <beniaminovagnarelli@gmail.com>
2022-05-07 19:35:36 +0200

    admin_panel

--- api/get_challenge_names.php
+++ api/get_challenge_names.php
@@ -1,13 +1,9 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 session_start();
 require "../inc/functions.php";
 $conn = db_connect();
 
-if($_SESSION["role"] != 'A') return "";
+if($_SESSION["role"] != 'A') return false;
 
 $query = "SELECT challenge_name FROM CTF_challenge";
 $result = $conn->query($query);
