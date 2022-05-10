ab9a0f6732b6c5869e34cc5e0dce257de910c421
feedz <feedz@pop-os.localdomain>
2022-05-10 08:34:25 +0200

    background

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ $conn = db_connect();
 $title = "CTF h4ckus4t1";
 require "inc/head.php";
 ?>
+
+<style>
+body {
+  background-image: url('img/pexels-photo-904690.jpg');
+}
+</style>
 <body>
     <?php require "inc/navbar.php"; ?>
     <div id="main">
